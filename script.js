68d4fee91d75e3de2dc1f9a8db8ac32d51e3b490
Manish Jaunjalkar <117558314+manishjaunjalkar@users.noreply.github.com>
2023-05-02 00:16:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function insert_Row() {
     //Write your code here
-  
+	let tr = document.createElement("tr");
+	tr.innerHTML = '<td>New Cell1</td><td>New Cell2</td>';
+	let tablerow= document.getElementById("sampleTable");
+	tablerow.prepend(tr);
   
 }
